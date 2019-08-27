e19ca8f4cfa7fc5727890414b0c2e909dd80163f
Bill Donahue <billd@incentum.network>
2019-08-27 19:38:19 +1200

    headers, copyright, license

--- src/tx.ts
+++ src/tx.ts
@@ -1,3 +1,24 @@
+/*
+ * Licensed to Incentum Ltd. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Incentum Ltd. licenses this file to you under
+ * the Token Use License Version 1.0 and the Token Use
+ * Clause (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *  https://github.com/incentum-network/tul/blob/master/LICENSE.md
+ *  https://github.com/incentum-network/tul/blob/master/TUC.md
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import axios from 'axios'
 import { get } from 'lodash';
 
@@ -66,6 +87,10 @@ export function arkUrl(): string {
 
 export function setNetwork(network: string) {
   arkNetwork = network;
+}
+export function setNetworkUrl(network: string, url: string) {
+  arkNetwork = network;
+  networks[arkNetwork].url = url
 }
 const BROADCAST_RETRIES = [8000, 8000, 8000, 8000, 8000, 8000, 8000]
 const responseUrl = (address) => `${arkUrl()}/api/v2/wallets/${address}`
